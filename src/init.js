c5b62aa79437106b76bdc796235f0cebe0b478ca
timon <timon0596@yandex.ru>
2020-11-06 08:21:32 +0300

    new chart 2 formula

--- src/init.js
+++ src/init.js
@@ -25,4 +25,17 @@ function newConfig(num = 1234) {
   return new_config;
 }
 $('.root2').myChart({ ...newConfig(123123), diapason: { full: false } });
-$('.root3').myChart({ ...newConfig(5000000), diapason: { full: true } });
+$('.root3').myChart({
+  ...newConfig(50000),
+  diapason: { full: true },
+  series: [
+    {
+      name: 'qwer',
+      data: new Array(5000000).fill(0).map((el, i) => (Math.sin(i * 0.001) * Math.exp(i * (0.000001))).toFixed(2)),
+    },
+  ],
+  x: {
+    categories: new Array(5000000).fill(0).map((el, i) => i + 1),
+    title: 'indexes',
+  },
+});
